d076802eb968bd5f9aff4aa1316917fdc4417b83
martinez116497 <61223879+martinez116497@users.noreply.github.com>
2023-03-06 20:19:02 +0100

    prueba17

--- WebXR_vr_dragging/js/main.js
+++ WebXR_vr_dragging/js/main.js
@@ -64,16 +64,17 @@ function init() {
     controller1.addEventListener( 'selectstart', onSelectStart );
     controller1.addEventListener( 'selectend', onSelectEnd );
     scene.add( controller1 );
-    
-    const controllerModelFactory = new XRControllerModelFactory();
-    controllerGrip1 = renderer.xr.getControllerGrip( 0 );
-    controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
-    scene.add( controllerGrip1 );
-         
+
     controller2 = renderer.xr.getController( 1 );
     controller2.addEventListener( 'selectstart', onSelectStart );
     controller2.addEventListener( 'selectend', onSelectEnd );
     scene.add( controller2 );
+
+    const controllerModelFactory = new XRControllerModelFactory();
+
+    controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+    controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+    scene.add( controllerGrip1 );
 
     controllerGrip2 = renderer.xr.getControllerGrip( 1 );
     controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
